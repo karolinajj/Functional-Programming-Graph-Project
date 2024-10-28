8cfd509bd0d27e467db7b2e0bf9c996087fe03a8
karolinajj <115790529+karolinajj@users.noreply.github.com>
2024-10-28 20:20:46 -0100

    Added travelSales function with its helpers

--- project1.hs
+++ project1.hs
@@ -45,23 +45,21 @@ createAdjList roadmap = foldr addEdge [] roadmap
       | c == city = (c, neighbor : neighbors) : rest
       | otherwise = (c, neighbors) : addNeighbor city neighbor rest
 
-minDirectDistance :: City -> City -> AdjList -> Maybe Distance -- Function to find the minimum direct distance between two cities
-minDirectDistance _ _ [] = Nothing
+
+minDirectDistance :: City -> City -> AdjList -> Int -- Function to find the minimum direct distance between two cities
+minDirectDistance _ _ [] = 10^9 -- returns a large value if cities are not connected
 minDirectDistance city1 city2 ((city, neighbors):rest)
-    | city == city1 = 
+    | city1 == city2 = 0
+    | city == city1 =
         case filter (\(neighbor, _) -> neighbor == city2) neighbors of
             [] -> minDirectDistance city1 city2 rest
-            distances -> Just (minimum (map snd distances))
+            distances -> minimum (map snd distances)
     | otherwise = minDirectDistance city1 city2 rest
 
-cityToInt :: City -> [City] -> Int
-cityToInt _ [] = -1
-cityToInt city cities = cityToInt_acc city cities 0
-  where
-    cityToInt_acc _ [] _ = -1
-    cityToInt_acc target (x:xs) acc
-      | target == x = acc
-      | otherwise = cityToInt_acc target xs (acc + 1)
+intToCity :: Int -> [City] -> City
+intToCity n cities
+  | n < 0 || n >= length cities = "Error: Index out of bounds"
+  | otherwise = cities !! n
 
 -- Functions requested by the project
 
@@ -93,9 +91,9 @@ pathDistance :: RoadMap -> Path -> Maybe Distance -- Calculates the distance bet
 pathDistance roads path
     | null path = Just 0                                                                          -- Case of empty path
     | length path == 1 = Just 0                                                                   -- Case of single city
-    | otherwise = 
+    | otherwise =
       case distance roads startCity nextCity of                                                   -- Calculate the distance if there is a road
-        Just dist -> 
+        Just dist ->
             case pathDistance roads (nextCity : tail (tail path)) of                              -- Recursive call for the rest of the path
                 Just total -> Just (dist + total)                                                 -- Sum the distances of the road with the rest
                 Nothing -> Nothing                                                                -- Marks the path as invalid
@@ -104,9 +102,7 @@ pathDistance roads path
     startCity = head path                                                                         -- Selects the first city on the path
     nextCity = head (tail path)                                                                   -- Selects the next city on the path 
 
---I assum that:
---1) in (city1, city2, dist) city1 /= city2
---2) in the roadmap there is only one triple for each edge
+--I assum that in the roadmap there is only one triple for each edge
 rome :: RoadMap -> [City]
 rome [] = []
 rome roadmap = map fst (maxOccurence (countOccurences (roadsFromCities roadmap) []) [])
@@ -119,7 +115,7 @@ rome roadmap = map fst (maxOccurence (countOccurences (roadsFromCities roadmap) 
     countOccurences [] acc = acc
     countOccurences (city:xs) acc = countOccurences filteredXs ((city, 1 + sameCity):acc)
       where
-        sameCity = length(filter (== city) xs)
+        sameCity = length (filter (== city) xs)
         filteredXs = filter (/= city) xs
 
     maxOccurence :: [(City, Int)] -> [(City, Int)] -> [(City, Int)]
@@ -169,10 +165,47 @@ shortestPath cityMap start target
       | otherwise = [path ++ [nextCity]]
 
     neighbors :: AdjList -> City -> [(City, Distance)]                                                        -- Function to get the list of the neighbors of a city when provided with the adjlist
-    neighbors adjList city = case lookup city adjList of                                                      
+    neighbors adjList city = case lookup city adjList of
       Just neighborsList -> neighborsList
       Nothing -> []
 
+
+travelSales :: RoadMap -> Path
+travelSales roadmap = 
+    if minimum costs < 10^9 
+    then intToCity start cities : findPath minIndex lastSubset 
+    else []
+  where
+    adjlist = createAdjList roadmap
+    cities = map fst adjlist
+    n = length cities
+    start = 0
+    lastSubset = ((2^(n-1)) - 1) `Data.Bits.shiftL` 1
+
+    memo = Data.Array.array ((0, 0), (n - 1, (1 `Data.Bits.shiftL` n) - 1)) 
+           [((end, mask), dp (end, mask)) | end <- [0..n-1], mask <- [0..(1 `Data.Bits.shiftL` n) - 1]]
+
+    costs = [memo Data.Array.! (i, lastSubset) + minDirectDistance (intToCity i cities) (intToCity start cities) adjlist | i <- [1..n-1]]
+    minIndex = snd $ minimum $ zip costs [1..n-1]
+
+    dp :: (Int, Int) -> Int
+    dp (end, 0) = minDirectDistance (intToCity end cities) (intToCity start cities) adjlist
+    dp (end, mask)
+      | mask == Data.Bits.setBit 0 end = minDirectDistance (intToCity end cities) (intToCity start cities) adjlist
+      | otherwise = minimum [memo Data.Array.! (j, Data.Bits.clearBit mask end) + minDirectDistance (intToCity j cities) (intToCity end cities) adjlist 
+                             | j <- citiesInSubset mask, j /= end]
+
+    citiesInSubset :: Int -> [Int]
+    citiesInSubset subset = [i | i <- [1..n-1], Data.Bits.testBit subset i]
+
+    findPath :: Int -> Int -> Path
+    findPath end 0 = [intToCity end cities]
+    findPath end mask
+      | mask == Data.Bits.setBit 0 end = [intToCity end cities, intToCity 0 cities]
+      | otherwise = intToCity end cities : findPath next (Data.Bits.clearBit mask end)
+      where
+        next = snd $ minimum [(memo Data.Array.! (j, Data.Bits.clearBit mask end) + minDirectDistance (intToCity j cities) (intToCity end cities) adjlist, j)
+                             | j <- citiesInSubset mask, j /= end]
 
 tspBruteForce :: RoadMap -> Path
 tspBruteForce = undefined -- only for groups of 3 people; groups of 2 people: do not edit this function
@@ -186,12 +219,6 @@ gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2",
 
 gTest3 :: RoadMap -- unconnected graph
 gTest3 = [("0","1",4),("2","3",2)]
-
-gTest4 :: RoadMap -- unconnected graph
-gTest4 = [("A", "B", 10), ("A", "C", 15), ("B", "C", 10), ("B", "D", 12), ("C", "D", 10)]
-
-edges :: RoadMap -- multiple paths
-edges = [("A", "B", 3),  ("A", "C", 1),  ("C", "B", 2),  ("A", "D", 2), ("D", "B", 1)]
 
 -- Some cities for tests
 cTest1 :: City
